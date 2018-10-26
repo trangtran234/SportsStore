0f07a8f01c76d9511bae89ed53d34b9bbea0085b
trangtran234 <tn-trang.tran@outlook.com.vn>
2018-10-26 17:20:49 +0700

    Bootstrap and Razor

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,10 +19,21 @@ namespace SportsStore.WebUI.Controllers
         }
         public ViewResult List(int page = 1)
         {
-            return View(repository.Products.
-                OrderBy(p => p.ProductID).
-                Skip((page - 1)* PageSize).
-                Take(PageSize));
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                } 
+            };
+            return View(model);
         }
     }
 }
