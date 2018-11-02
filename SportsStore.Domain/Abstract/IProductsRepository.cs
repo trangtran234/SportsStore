b680e8207257e4e8af6c865ef21bdbb0eb0a41f9
trangtran234 <tn-trang.tran@outlook.com.vn>
2018-11-02 13:35:18 +0700

    Revert "Administrator Page: CRUD - finished"
    This reverts commit 6100caff0a9bc9ac3d9627ed34e4ee86c6b79197.

--- SportsStore.Domain/Abstract/IProductsRepository.cs
+++ SportsStore.Domain/Abstract/IProductsRepository.cs
@@ -11,6 +11,5 @@ namespace SportsStore.Domain.Abstract
     {
         IEnumerable<Product> Products { get; }
         void SaveProduct(Product product);
-        Product DeleteProduct(int productID);
     }
 }
